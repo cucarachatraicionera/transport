7d4ec29614676b2f9bd04330c27143fc42297783
blinddoggy <cucarachatraicionera@gmail.com>
2025-01-16 10:29:05 -0500

    adding number new

--- pages/index.js
+++ pages/index.js
@@ -204,7 +204,7 @@ const Home = ({
 
       {/* Botón flotante de WhatsApp */}
       <a
-        href="https://wa.me/573116918913" // Cambia al número de WhatsApp con el código de país.
+        href="https://wa.me/34624409340" // Cambia al número de WhatsApp con el código de país.
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-5 right-5 z-50 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
